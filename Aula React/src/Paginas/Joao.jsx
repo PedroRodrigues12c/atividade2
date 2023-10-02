083f06be3fec1075d061e573bc9174a21a515275
pedrodutra <henriquepedrodutra@gmail.com>
2023-10-02 16:57:38 -0300

    atividade2

---
+++ Aula React/src/Paginas/Joao.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+function Joao(){
+    return(
+        <div>
+            <h1>Ao mussar</h1>
+            <Link to={'/'}>Já pode ao mussar?</Link>
+        </div>
+    )
+}
+
+export default Joao
